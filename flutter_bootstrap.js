a3e93b40ec44e30195a3ad2c131eba7c77c3fc92
vsdos_mac <admin@192.168.1.13>
2025-07-28 02:22:51 +0300

    new build with sounds

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1978897474"
+    serviceWorkerVersion: "3546400867"
   }
 });
